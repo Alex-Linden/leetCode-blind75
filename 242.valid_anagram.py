5405b25a11267ee3b8c0cf509fadc568fd41cc76
Alex Linden <alextl2004@gmail.com>
2022-09-20 15:02:46 -0700

    python solution for 242

---
+++ 242.valid_anagram.py
@@ -0,0 +1,20 @@
+"""Given two strings s and t, return true if t is an anagram of s,
+ and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the
+letters of a different word or phrase, typically using all
+ the original letters exactly once."""
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count_s, count_t = {}, {}
+
+        for i in range(len(s)):
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[t[i]] = 1 + count_t.get(t[i], 0)
+
+        return count_s == count_t
