536113d8bd2bbe013271b8b216c5b6ccbf056fca
Alex Linden <alextl2004@gmail.com>
2022-09-28 10:51:16 -0700

    reorder list

---
+++ 143_reorder_list.js
@@ -0,0 +1,49 @@
+/**You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ */
+
+var reorderList = function (head) {
+  let secondHalf = getMid(head);
+  secondHalf = reverseLL(secondHalf);
+  reorder(head, secondHalf);
+
+};
+
+
+function getMid(head) {
+  let fast = head;
+  let slow = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+}
+
+function reverseLL(head) {
+  let [prev, curr, next] = [null, head, null];
+
+  while (curr) {/* Time O(N) */
+    next = curr.next;
+    curr.next = prev;
+
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
+
+function reorder(list1, list2) {
+  while (list2) {
+    let tmp = list2.next;
+    list2.next = list1.next;
+    list1.next = list2;
+    list2 = tmp;
+  }
+}
