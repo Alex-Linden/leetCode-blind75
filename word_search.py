165757556d763febfd1a7ffd975723670c129657
Alex Linden <alextl2004@gmail.com>
2022-10-26 15:46:23 -0700

    words and words and more

--- word_search.py
+++ word_search.py
@@ -1,7 +1,7 @@
-"""Write an algorithm to find if the given word
-occurs in the grid on a row or a column,
-forward or backwards.
-Input
+"""Write an algorithm to find if the given word occurs in the grid
+on a row or a column, forward or backwards.
+
+ Input
 The first line of input consists of two integers-
 grid_ row and grid col, representing the
 number of rows (N) and the number of
@@ -40,28 +40,89 @@ first row, "TOM" is found at last column,
 So, the output is ["Yes", "Yes", "Yes" "No"].
 """
 
+
 def word_search(grid, words):
+    row, col = 0, 0
 
+    res = []
 
+    for word in words:
+        word_not_found = True
+        while row < len(grid) and word_not_found:
+            while col < len(grid[0]):
+                if grid[row][col] == word[0]:
+                    if has_word(grid, word, [row, col]):
+                        res.append("Yes")
+                        word_not_found = False
+                elif grid[row][col] == word[-1]:
+                    if has_word(grid, word[::-1], [row, col]):
+                        res.append("Yes")
+                        word_not_found = False
+
+                col += 1
+
+            row += 1
+            col = 0
+
+        if word_not_found:
+            res.append("No")
+
+        row, col = 0, 0
+        print(res)
+
+    return res
 
 
 def has_word(grid, word, position):
-    x, y = position
+    row, col = position[0], position[1]
     i = 0
 
-    while x < len(word) + x and x < len(grid[0]):
+    horiz_word = ""
 
+    # iterate through a row and check if word is there
+    while  col < len(grid[0]):
+        if grid[row][col] == word[i]:
+            horiz_word += grid[row][col]
+        else:
+            break
 
-        if not grid[x][y] == word[i]:
-            return False
-
-        x += 1
+        col += 1
         i += 1
 
-    while y < len(word) + y and :
+    if horiz_word == word:
+        return True
+
+    # reset i and col
+    i = 0
+    col = position[1]
+    # initialize word for vertical comparison
+    vert_word = ""
+
+    while row < len(grid):
+        if grid[row][col] == word[i]:
+            vert_word += grid[row][col]
+            print(vert_word)
+        else:
+            return False
+
+        row += 1
+        i += 1
+
+    return vert_word == word
+
+
+
+# [["C", "A", "T"],
+# ['I', 'D', 'O'],
+# ['N', 'O', 'M']]
+
+# ['CAT', 'TOM', 'ADO', 'MOM']
 
 
 
 
+# len(grid[0]) - col >= len(word) - i and i < len(word):
 
-    return False
+
+
+# len(grid) - row >= len(word) - i and i < len(word):
